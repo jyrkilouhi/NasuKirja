10fd20ca34559150268155d86f3641b2855fcb4a
Jyrki Louhi <jyrki.louhi@gmail.com>
2019-04-14 10:21:07 +0300

    Lisätty muutama virheen tarkistus.

--- src/main/java/projekti/UserPageController.java
+++ src/main/java/projekti/UserPageController.java
@@ -1,5 +1,6 @@
 package projekti;
 
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class UsersController {
+public class UserPageController {
     
     @Autowired
     private AccountRepository accountRepository;
@@ -22,22 +23,38 @@ public class UsersController {
     private FriendsRepository friendRepository;
     
     @GetMapping("/kayttajat")
-    public String view() {
+    public String view(Model model) {
+        model.addAttribute("loggedUser", authenticationName());
         return "userform";
     }
     
     @PostMapping("/kayttajat")
     public String find(Model model, String findname) {
         model.addAttribute("loggedUser", authenticationName());
-        model.addAttribute("userlist", accountRepository.findByRealnameContaining(findname));
+        List <Account> foundUsers = accountRepository.findByRealnameContaining(findname);
+        if(foundUsers.size() == 0) {
+            model.addAttribute("FindUserError", "Antamallasi hakuehdolla " + findname + " ei löydy yhtään käyttäjää");
+            return "userform";                
+        }
+        if(foundUsers.size() > 50) {
+            model.addAttribute("FindUserError", "Antamallasi hakuehdolla " + findname + " löytyi " + foundUsers.size() + " käyttäjää. Tarkenna hakua.");
+            return "userform";                
+        }        
+        model.addAttribute("userlist", foundUsers);
         return "userform";
     }
     
     @GetMapping("/kayttajat/{profilename}")
     public String view(Model model, @PathVariable String profilename) {
         model.addAttribute("loggedUser", authenticationName());
-        model.addAttribute("user", accountRepository.findByProfilename(profilename));
-        return "userpage";
+        Account user = accountRepository.findByProfilename(profilename);
+        if(user != null) {
+            model.addAttribute("user", user);
+            return "userpage";
+        } else {
+            model.addAttribute("FindUserError", "Antamaasi profiilia " + profilename + " ei löydy");
+            return "userform";            
+        }
     }
     
     private String authenticationName() {
