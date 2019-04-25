b335bdea4c1305bd695cf4135770cbacac798eb5
Jyrki Louhi <jyrki.louhi@gmail.com>
2019-04-25 18:07:37 +0300

    Toimiva Heroku versio

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -20,13 +20,13 @@ public class DefaultController {
         return "index";
     }
     
-//    @GetMapping
-//    public String handlingNonDefinedPathsToErrorPage(HttpServletRequest request, Model model) {
-//        model = accountService.addAuthenticationName(model);  
-//        model.addAttribute("numberOfAccounts", accountService.numberOfAccounts());
-//        model.addAttribute("errorURl", request.getRequestURI());
-//        return "error";
-//    }
+    @GetMapping
+    public String handlingNonDefinedPathsToErrorPage(HttpServletRequest request, Model model) {
+        model = accountService.addAuthenticationName(model);  
+        model.addAttribute("numberOfAccounts", accountService.numberOfAccounts());
+        model.addAttribute("errorURl", request.getRequestURI());
+        return "terror";
+    }
     
     @GetMapping("/help")
     public String viewHelpPage(Model model) {
